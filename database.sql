e69d4e109f442f445c67e4131b9aacf9f26494ac
Javier Aranda <j.arandaizquierdo@gmail.com>
2015-12-22 01:44:21 +0100

    Creada la tabla usuario a falta de chekear si esta correcta

--- database.sql
+++ database.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE IF NOT EXISTS Coworking;
+#Creamos una nueva base de datos llamada Coworking
+CREATE DATABASE IF NOT EXISTS coworking;
+
+#Creacion de la tabla de usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
+      DNI VARCHAR(6) PRIMARY KEY,
+      nombre VARCHAR(20) NOT NULL,
+      apellido VARCHAR(20) NOT NULL,
+      telefono VARCHAR(9) NOT NULL,
+      direccion VARCHAR(50) NOT NULL,
+      tipo VARCHAR CHECK (tipo == "Invitado" || tipo == "Individual" || tipo == "Empresa"),
+      tarifa VARCHAR CHECK (tarifa == "Dia" || tarifa == "Semana" || tarifa == "mes")
+);
